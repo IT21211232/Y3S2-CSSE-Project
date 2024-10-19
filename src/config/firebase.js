1973fca9d2b56b55a5574dc6a56b3f392fdc8211
shafdo <yugantha1468@gmail.com>
2024-10-19 12:38:53 +0530

    Feat: Register & Login integrated

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,16 +1,19 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from '@firebase/firestore'
+import { getFirestore } from "@firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAzw3cWjAlu2ArWZlU6zNwYepKjYXT31jc",
-    authDomain: "csse-assignment2.firebaseapp.com",
-    projectId: "csse-assignment2",
-    storageBucket: "csse-assignment2.appspot.com",
-    messagingSenderId: "54569636602",
-    appId: "1:54569636602:web:b436954b1870fde729ef43",
-    measurementId: "G-HBC9QL6SGM"
-  };
+  apiKey: "AIzaSyAzw3cWjAlu2ArWZlU6zNwYepKjYXT31jc",
+  authDomain: "csse-assignment2.firebaseapp.com",
+  projectId: "csse-assignment2",
+  storageBucket: "csse-assignment2.appspot.com",
+  messagingSenderId: "54569636602",
+  appId: "1:54569636602:web:b436954b1870fde729ef43",
+  measurementId: "G-HBC9QL6SGM",
+};
 
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
 
-export const db = getFirestore();
+export { db, auth };
